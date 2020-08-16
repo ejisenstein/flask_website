1042f73b53f38c236be1970a63cc89bf810d6be9
Evan Isenstein <ejisenstein@gmail.com>
2020-08-16 14:44:09 -0400

    created ed part of site

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/edu")
+def bio():
+    return render_template("edu.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
